3d594642e507f2f32f97b1c3f30910c3ac4c0efa
vijay gohel <gohil5898@gmail.com>
2021-03-05 10:21:44 +0530

    recipe-list and recipe-detail

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
+  recipes: Recipe[] =[
+    new Recipe('A test recipe','This is a test','https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg'),
+    new Recipe('A test recipe','This is a test','https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg'),
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
